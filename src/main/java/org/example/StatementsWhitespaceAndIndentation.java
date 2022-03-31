cc5ecd90f23bba9ccc2df6b0f89cbe53a2620e96
NegarMadadi <madadi.negar@gmail.com>
2022-03-31 21:39:52 +0200

    Created StatementsWhitespaceAndIndentation.java

---
+++ src/main/java/org/example/StatementsWhitespaceAndIndentation.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+public class StatementsWhitespaceAndIndentation {
+    public static void main(String[] args) {
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is " +
+                "another" +
+                "plus more.");
+
+
+    }
+
+
+}
