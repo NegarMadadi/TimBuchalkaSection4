e10d30512365f3764c496abb141b42fe57370a31
NegarMadadi <madadi.negar@gmail.com>
2022-04-01 21:03:57 +0200

    Created LeapYearCalculator

---
+++ src/main/java/org/example/LeapYearCalculator.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class LeapYearCalculator {
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(1924));
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year >= 1 && year <= 9999) {
+            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+}
