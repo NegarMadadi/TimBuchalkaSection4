8ce386e398e373ec4f6178a36778a17ceeecf0e2
NegarMadadi <madadi.negar@gmail.com>
2022-04-01 18:07:28 +0200

    Created BarkingDog.java

---
+++ src/main/java/org/example/BarkingDog.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class BarkingDog {
+    public static void main(String[] args) {
+
+        System.out.println(shouldWakeUp(true, 1));
+        System.out.println(shouldWakeUp(false, 2));
+
+    }
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (hourOfDay < 0 || hourOfDay > 23) {
+            return false;
+        } else if ((barking == true) && (hourOfDay < 8 || hourOfDay > 22)) {
+            return true;
+        }
+        return false;
+    }
+}
