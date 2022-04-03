930de63783a714a84824d25589a902abb2fd4871
NegarMadadi <madadi.negar@gmail.com>
2022-04-03 17:58:06 +0200

    creating constants and some string changes in the code.

--- src/main/java/org/example/SecondAndMinuteChallenge.java
+++ src/main/java/org/example/SecondAndMinuteChallenge.java
@@ -1,22 +1,48 @@
 package org.example;
 
 public class SecondAndMinuteChallenge {
+    private static final  String Invalid_value_message = "Invalid value";
+
     public static void main(String[] args) {
         System.out.println(getDurationString(65, 45));
         System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(-32));
+        System.out.println(getDurationString(65, 9));
     }
 
     public static String getDurationString(int minutes, int seconds) {
+
         int hour;
-        if ((minutes >= 0) && (seconds >= 0 && seconds <= 59)) {
-            hour = minutes / 60;
-            minutes = minutes % 60;
-        } else return "Invalid value";
-        return (hour + "h " + minutes + "m " + seconds + "s");
+        if ((minutes < 0) || ((seconds < 0) || (seconds > 59))) {
+            return Invalid_value_message;
+        }
+        hour = minutes / 60;
+        minutes = minutes % 60;
+
+        String hoursString = hour + "h";
+        if (hour < 10) {
+            hoursString = "0" + hoursString;
+        }
+
+        String minutesString = minutes + "m";
+        if (minutes < 10) {
+            minutesString = "0" + minutesString;
+        }
+
+
+        String secondsString = seconds + "s";
+        if (seconds < 10) {
+            secondsString = "0" + secondsString;
+        }
+
+
+        return (hoursString + " " + minutesString + " " + secondsString);
     }
+
+
     public static String getDurationString(int seconds) {
         if ((seconds <= 0)) {
-            return "Invalid value";
+            return Invalid_value_message;
         } else {
             int minutes = seconds / 60;
             int remainingSeconds = seconds % 60;
